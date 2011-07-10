172329fa9141339a7a26652d74b8e2a92c4942ba
gary4gar <contact@gaurishsharma.com>
2011-07-10 14:29:54 -0700

    Improved Comments in Core.php

--- app/core.php
+++ app/core.php
@@ -14,7 +14,9 @@ require_once('load.php');
 
 /*
 * model class is usally used to handle database acess.
-* basically its datasource for our other class
+* basically its datasource for our other class.
+* Currently, its empty. Feel free to place database connection code in Model
+* This is where it should go
 */
 require_once('model.php');
 
@@ -22,12 +24,13 @@ require_once('model.php');
 * Controller Class is the main glue between model & views
 * All requests from user are routed to controller
 * Then its job of controller to decide which view or model to load 
-* Basically controller is THE GUY 
+* Basically controller is THE GUY which everyone on what to do
 */
 require_once('controller.php');
 
 /*
 * Start our app
 * As we are done including needed files, lets start
+* Fires up new instance of Controller class from controller.php
 */
 new Controller();
