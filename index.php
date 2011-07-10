5593ca94f2f4807f36e1f12f057aa9c73bb848c3
gary4gar <contact@gaurishsharma.com>
2011-07-10 14:21:47 -0700

    Removed Whitespace

--- index.php
+++ index.php
@@ -21,7 +21,6 @@ require_once('app/core.php');
 * The closing tag(?>) is omitted on purpose. In PHP only source files, It
 * is not required by PHP, and omitting it prevents the accidental
 * injection of trailing white space into  the response
-
 [1] http://framework.zend.com/manual/en/coding-standard.php-file-formatting.html
 [2] http://www.sitepoint.com/should-you-close-your-php-code-tags/
 *
